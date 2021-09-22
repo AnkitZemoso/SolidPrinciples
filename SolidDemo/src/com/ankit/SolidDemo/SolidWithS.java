313aa8b4f6ed96c3862755074c4209fae7903bd9
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-22 15:10:34 +0530

    Add files via upload

---
+++ SolidDemo/src/com/ankit/SolidDemo/SolidWithS.java
@@ -0,0 +1,12 @@
+package com.ankit.SolidDemo;
+
+public class SolidWithS {
+    // We can make a new class to transfer the printing function
+    class Book{
+        public void addBookToDB(){
+
+        }
+
+
+    }
+}
